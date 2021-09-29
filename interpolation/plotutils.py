d86402288d67ebd4bdfdb4e2cee1f5deb8233240
Norberto Goizueta <ng213@ic.ac.uk>
2021-09-29 17:02:26 +0200

    New utils and plotutils

---
+++ interpolation/plotutils.py
@@ -0,0 +1,63 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import interpolation.interface as interface
+
+
+def plot_optimisation_data(optimisation_info, optimisation_id, opt=None):
+    path_to_data = optimisation_info.optimisation_data[optimisation_id]['output_directory']
+    if opt is None:
+        report_file = path_to_data + f'/evaluations_report.txt'
+
+        evaluations = np.loadtxt(report_file, skiprows=1)
+        iter_id = evaluations[:, 0]
+        cost = -evaluations[:, 1]
+        param_values = evaluations[:, 2:]
+    else:
+        cost = -opt.Y
+        param_values = opt.X
+    source_points = param_values[cost == 0.]
+    n_source_points = source_points.shape[0]
+
+    training_data = interface.yaml_to_array(path_to_data + '/training_data.yaml')
+
+    plt.figure()
+    cmap = plt.get_cmap('Reds')
+    nrm = plt.Normalize(vmin=0, vmax=np.ceil(np.max(cost)))
+    plt.scatter(training_data[:, 0], training_data[:, 1], marker='x', color='k')
+
+    plt.plot(param_values[n_source_points:, 0], param_values[n_source_points:, 1], ls='--')
+    plt.scatter(param_values[n_source_points:, 0], param_values[n_source_points:, 1], marker='o',
+                facecolor=cmap(nrm(cost[n_source_points:])), edgecolor='k', lw=0.2)
+    plt.colorbar(mpl.cm.ScalarMappable(nrm, cmap), label='Cost Function')
+
+    plt.text(param_values[n_source_points + 1, 0], param_values[n_source_points + 1, 1], '1')
+    plt.text(param_values[-1, 0], param_values[-1, 1], 'end')
+    plt.savefig(path_to_data + '/optimisation_path2d.pdf')
+
+    fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+    ax.plot(param_values[n_source_points:, 0], param_values[n_source_points:, 1], ls='--')
+    ax.scatter(param_values[:n_source_points, 0], param_values[:n_source_points, 1], cost[:n_source_points],
+               marker='o', facecolor='k')
+    ax.scatter(param_values[n_source_points:, 0], param_values[n_source_points:, 1], cost[n_source_points:],
+               marker='o', facecolor='r')
+    for i_point in range(n_source_points, param_values.shape[0]):
+        ax.plot([param_values[i_point, 0], param_values[i_point, 0]],
+                [param_values[i_point, 1], param_values[i_point, 1]], [0., cost[i_point]], color='tab:blue')
+    ax.set_xlabel('Angle of Attack')
+    ax.set_ylabel('Free stream velocity')
+    ax.set_zlabel('Cost function')
+    plt.savefig(path_to_data + '/optimisation_path3d.pdf')
+
+
+def plot_acquisition_functions(optimisation_info, optimisation_id, optimiser):
+    path_to_data = optimisation_info.optimisation_data[optimisation_id]['output_directory']
+
+    fig = plt.figure()
+    optimiser.plot_acquisition(label_x='Angle of Attack', label_y='Free stream velocity')
+    plt.tight_layout()
+    plt.savefig(path_to_data + '/optimisation_acquisition.pdf')
+
+    fig = plt.figure()
+    optimiser.plot_convergence()
+    plt.savefig(path_to_data + '/optimisation_convergence.pdf')
---
+++ interpolation/utils.py
@@ -0,0 +1,94 @@
+import numpy as np
+import sharpytools.linear.stability as stability
+
+
+def produce_root_locus(dataset, param, value):
+    list_of_constant_cases, params = retrieve_constant_parameter_cases(param, value, dataset.aeroelastic)
+
+    eigs = []
+    param_array = []
+
+    for case in list_of_constant_cases:
+        try:
+            param_array.append(np.ones((case.eigs.shape[0], len(case.parameter_value))) * case.parameter_value)
+            eigs.append(case.eigs)
+        except TypeError:
+            param_array.append(np.ones_like(case.eigs[:, 0]) * case.parameter_value)
+        except AttributeError:
+            continue
+
+    param_array = np.concatenate(param_array)
+    eigs = np.concatenate(eigs)
+
+    try:
+        u_inf_idx = dataset.parameter_name.index('u_inf')
+    except AttributeError:
+        u_inf_idx = dataset.param_name.index('u_inf')
+
+    return param_array[:, u_inf_idx], eigs
+
+
+def produce_vgf(dataset, param, value, **kwargs):
+
+    list_of_constant_cases, params = retrieve_constant_parameter_cases(param, value, dataset.aeroelastic)
+
+    eigs = []
+    param_array = []
+
+    for case in list_of_constant_cases:
+        try:
+            param_array.append(np.ones((case.eigs.shape[0], len(case.parameter_value))) * case.parameter_value)
+            eigs.append(case.eigs)
+        except TypeError:
+            param_array.append(np.ones_like(case.eigs[:, 0]) * case.parameter_value)
+        except AttributeError:
+            continue
+
+    param_array = np.concatenate(param_array)
+    eigs = np.concatenate(eigs)
+
+    try:
+        u_inf_idx = dataset.parameter_name.index('u_inf')
+    except AttributeError:
+        u_inf_idx = dataset.param_name.index('u_inf')
+    vel, damp, fn = stability.modes(param_array[:, u_inf_idx],
+                                    eigs, use_hz=True, wdmax=50 * 2 * np.pi,
+                                    **kwargs)
+
+    return vel, damp, fn
+
+
+def compute_flutter(dataset, param, value, instability_damping=0., vel_vmin=0., **kwargs):
+
+    v, damp, _ = produce_vgf(dataset, param, value, **kwargs)
+
+    return stability.find_flutter_speed2(v, damp, instability_damping, vel_vmin)
+    # return stability.find_flutter_speed(v, damp, instability_damping, vel_vmin)
+
+
+def retrieve_constant_parameter_cases(param, value, dataset):
+    """
+
+    Args:
+        param: name
+        value: constant value
+        dataset: Actual/Interpolated.aeroelastic
+
+    Returns:
+
+    """
+
+    list_of_cases = []
+    param_value = []
+
+    for case in dataset:
+        if case.case_info[param] == value:
+            list_of_cases.append(case)
+            param_value.append(case.case_info['u_inf'])  # for now....
+
+    # sort them out
+    order = np.argsort(np.array(param_value))
+    list_of_cases = [list_of_cases[i_order] for i_order in order]
+    param_value = np.array([param_value[i_order] for i_order in order])
+
+    return list_of_cases, param_value
